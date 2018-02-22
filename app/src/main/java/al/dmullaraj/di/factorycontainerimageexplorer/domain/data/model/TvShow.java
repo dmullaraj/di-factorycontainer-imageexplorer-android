fd9440b836633c29dac09f846a7227868d00d2cd
dmullaraj <d.mullaraj@gmail.com>
2018-02-22 11:41:07 +0100

    - added custom equals and hascode generator in order to support comparing tvshow's object type in future and also having them ready for hash (set-map-tree) datastructure

--- app/src/main/java/al/dmullaraj/di/factorycontainerimageexplorer/domain/data/model/TvShow.java
+++ app/src/main/java/al/dmullaraj/di/factorycontainerimageexplorer/domain/data/model/TvShow.java
@@ -5,24 +5,25 @@ import android.os.Parcelable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by denis.mullaraj on 22/02/2018.
  */
 
-public class TvShow implements Parcelable {
+public class TvShow implements Parcelable{
 
     private String poster_path;
-    private double popularity;
-    private int id;
+    private Double popularity;
+    private Integer id;
     private String backdrop_path;
-    private double vote_average;
+    private Double vote_average;
     private String overview;
     private String first_air_date;
     private List<String> origin_country;
     private List<Integer> genre_ids;
     private String original_language;
-    private int vote_count;
+    private Integer vote_count;
     private String name;
     private String original_name;
     private final String base_url = "https://image.tmdb.org/t/p/";
@@ -141,26 +142,26 @@ public class TvShow implements Parcelable {
 
     protected TvShow(Parcel in) {
         poster_path = in.readString();
-        popularity = in.readDouble();
-        id = in.readInt();
+        popularity = in.readByte() == 0x00 ? null : in.readDouble();
+        id = in.readByte() == 0x00 ? null : in.readInt();
         backdrop_path = in.readString();
-        vote_average = in.readDouble();
+        vote_average = in.readByte() == 0x00 ? null : in.readDouble();
         overview = in.readString();
         first_air_date = in.readString();
         if (in.readByte() == 0x01) {
-            origin_country = new ArrayList<String>();
+            origin_country = new ArrayList<>();
             in.readList(origin_country, String.class.getClassLoader());
         } else {
             origin_country = null;
         }
         if (in.readByte() == 0x01) {
-            genre_ids = new ArrayList<Integer>();
+            genre_ids = new ArrayList<>();
             in.readList(genre_ids, Integer.class.getClassLoader());
         } else {
             genre_ids = null;
         }
         original_language = in.readString();
-        vote_count = in.readInt();
+        vote_count = in.readByte() == 0x00 ? null : in.readInt();
         name = in.readString();
         original_name = in.readString();
     }
@@ -173,10 +174,25 @@ public class TvShow implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(poster_path);
-        dest.writeDouble(popularity);
-        dest.writeInt(id);
+        if (popularity == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeDouble(popularity);
+        }
+        if (id == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeInt(id);
+        }
         dest.writeString(backdrop_path);
-        dest.writeDouble(vote_average);
+        if (vote_average == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeDouble(vote_average);
+        }
         dest.writeString(overview);
         dest.writeString(first_air_date);
         if (origin_country == null) {
@@ -192,7 +208,12 @@ public class TvShow implements Parcelable {
             dest.writeList(genre_ids);
         }
         dest.writeString(original_language);
-        dest.writeInt(vote_count);
+        if (vote_count == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeInt(vote_count);
+        }
         dest.writeString(name);
         dest.writeString(original_name);
     }
@@ -209,4 +230,47 @@ public class TvShow implements Parcelable {
             return new TvShow[size];
         }
     };
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if(!(obj instanceof TvShow)) return false;
+
+        TvShow tvShow = (TvShow) obj;
+
+        if(poster_path != null ? !poster_path.equals(tvShow.poster_path) : tvShow.poster_path != null) return false;
+        if(popularity != null ? !popularity.equals(tvShow.popularity) : tvShow.popularity != null) return false;
+        if(id != null ? !id.equals(tvShow.id) : tvShow.id != null) return false;
+        if(backdrop_path != null ? !backdrop_path.equals(tvShow.backdrop_path) : tvShow.backdrop_path != null) return false;
+        if(vote_average != null ? !vote_average.equals(tvShow.vote_average) : tvShow.vote_average != null) return false;
+        if(overview != null ? !overview.equals(tvShow.overview) : tvShow.overview != null) return false;
+        if(first_air_date != null ? !first_air_date.equals(tvShow.first_air_date) : tvShow.first_air_date != null) return false;
+        if(origin_country != null ? !origin_country.equals(tvShow.origin_country) : tvShow.origin_country != null) return false;
+        if(genre_ids != null ? !genre_ids.equals(tvShow.genre_ids) : tvShow.genre_ids != null) return false;
+        if(original_language != null ? !original_language.equals(tvShow.original_language) : tvShow.original_language != null) return false;
+        if(vote_count != null ? !vote_count.equals(tvShow.vote_count) : tvShow.vote_count != null) return false;
+        if(name != null ? !name.equals(tvShow.name) : tvShow.name != null) return false;
+        if(original_name != null ? !original_name.equals(tvShow.original_name) : tvShow.original_name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                poster_path,
+                popularity,
+                id,
+                backdrop_path,
+                vote_average,
+                overview,
+                first_air_date,
+                origin_country,
+                genre_ids,
+                original_language,
+                vote_count,
+                name,
+                original_name);
+    }
 }
